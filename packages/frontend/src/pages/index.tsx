29c252a179c323c84b30656e912c7895ead48f1d
Josef Vacek <vacekj@outlook.com>
2023-03-01 22:12:34 -0700

    wip

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,29 +1,12 @@
-import { HomePageTitle } from '@components/home/HomePageTitle'
-import { HomeTopBar } from '@components/home/HomeTopBar'
-import { CenterBody } from '@components/layout/CenterBody'
-import { GreeterContractInteractions } from '@components/web3/GreeterContractInteractions'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
-import type { NextPage } from 'next'
-import 'twin.macro'
+import {ConnectButton} from "@rainbow-me/rainbowkit";
+import type {NextPage} from "next";
 
 const HomePage: NextPage = () => {
-  return (
-    <>
-      {/* Top Bar */}
-      <HomeTopBar />
+    return (
+        <>
+            <ConnectButton/>
+        </>
+    );
+};
 
-      <CenterBody tw="mt-20 mb-10 px-5">
-        {/* Title */}
-        <HomePageTitle />
-
-        {/* Rainbowkit Connect Button */}
-        <ConnectButton />
-
-        {/* Greeter.sol Contract Interactions */}
-        <GreeterContractInteractions />
-      </CenterBody>
-    </>
-  )
-}
-
-export default HomePage
+export default HomePage;
--- packages/frontend/src/shared/wagmiClient.ts
+++ packages/frontend/src/shared/wagmiClient.ts
@@ -1,52 +1,56 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { allChains, chain, Chain, configureChains, createClient } from 'wagmi'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import { publicProvider } from 'wagmi/providers/public'
-import { env } from './environment'
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { allChains, chain, Chain, configureChains, createClient } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
+import { env } from "./environment";
 
 /**
  * Wagmi.sh Configuration (https://wagmi.sh/docs)
  */
 
 export const defaultChain: Chain | undefined = allChains.find(
-  (chain) => env.defaultChain === chain.id,
-)
+	(chain) => env.defaultChain === chain.id,
+);
 
 export const isChainSupported = (chainId?: number): boolean => {
-  return chainId && env.supportedChains.includes(chainId)
-}
-export const supportedChains: Chain[] = allChains.filter((chain) => isChainSupported(chain.id))
+	return chainId && env.supportedChains.includes(chainId);
+};
+export const supportedChains: Chain[] = allChains.filter((chain) =>
+	isChainSupported(chain.id),
+);
 
 export const getRpcUrl = (chainId: number): string => {
-  return env.rpcUrls[chainId as keyof typeof env.rpcUrls]
-}
+	return env.rpcUrls[chainId as keyof typeof env.rpcUrls];
+};
 
 export const {
-  chains: [, ...chains],
-  provider,
+	chains: [, ...chains],
+	provider,
 } = configureChains(
-  Array.from(new Set([chain.mainnet, defaultChain, ...supportedChains])).filter(Boolean) as Chain[],
-  [
-    jsonRpcProvider({
-      rpc: (chain) => {
-        const rpcUrl = getRpcUrl(chain.id)
-        if (!rpcUrl) {
-          throw new Error(`No RPC provided for chain ${chain.id}`)
-        }
-        return { http: rpcUrl }
-      },
-    }),
-    publicProvider(),
-  ],
-)
+	Array.from(new Set([chain.mainnet, defaultChain, ...supportedChains])).filter(
+		Boolean,
+	) as Chain[],
+	[
+		jsonRpcProvider({
+			rpc: (chain) => {
+				const rpcUrl = getRpcUrl(chain.id);
+				if (!rpcUrl) {
+					throw new Error(`No RPC provided for chain ${chain.id}`);
+				}
+				return { http: rpcUrl };
+			},
+		}),
+		publicProvider(),
+	],
+);
 
 const { connectors } = getDefaultWallets({
-  appName: 'ETHathon',
-  chains,
-})
+	appName: "change",
+	chains,
+});
 
 export const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-})
+	autoConnect: true,
+	connectors,
+	provider,
+});
