cf52d31ab526a02b5421e7a1cf77e5de8fea3a9b
Josef Vacek <vacekj@outlook.com>
2023-03-01 23:59:25 -0700

    wagmi working

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -5,6 +5,13 @@ const HomePage: NextPage = () => {
     return (
         <>
             <ConnectButton/>
+            <ol>
+                <li>1. Generate an identity</li>
+                <li>2. Join a group</li>
+                <li>3. Generate a proof of a vote</li>
+                <li>4. Submit the vote</li>
+                <li>5. Inspect the vote count</li>
+            </ol>
         </>
     );
 };
